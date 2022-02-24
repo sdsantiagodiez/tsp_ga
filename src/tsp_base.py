1af766e3f6dfe2a7f49891e292711ae97cfdb10b
Santiago Diez <sdsantiagodiez@gmail.com>
2022-02-24 08:44:29 -0300

    run method

--- src/tsp_base.py
+++ src/tsp_base.py
@@ -30,13 +30,12 @@ class TSP(object):
         self.gene_size = self.city_data.num_cities
         self.max_mutation_rate = max_mutation_rate
         self.uniform_population_mutation_rate = uniform_population_mutation_rate
-        self.__MAX_SELECTION_THRESHOLD = self.population_size / 2
+        self.__MAX_SELECTION_THRESHOLD = self.population_size // 2
         self.selection_threshold = (
             self.__MAX_SELECTION_THRESHOLD
             if not selection_threshold
             else selection_threshold
         )
-        self.__initialize_populations()
 
     @property
     def generation_number(self):
@@ -95,7 +94,11 @@ class TSP(object):
 
     @selection_threshold.setter
     def selection_threshold(self, value: float):
-        if not (value < self.__MIN_SELECTION_THRESHOLD):
+        if not (
+            self.__MIN_SELECTION_THRESHOLD
+            <= value
+            <= self.__MAX_SELECTION_THRESHOLD
+        ):
             value_error_message = "Selection threshold can't be between "
             raise ValueError(
                 f"{value_error_message} {self.__MIN_SELECTION_THRESHOLD} \n"
@@ -155,9 +158,9 @@ class TSP(object):
         self.populations = self.__get_new_populations()
         self.populations_mutation_rate = self.__get_populations_mutation_rates()
 
-    def __calculate_fitness(self, gene: np.ndarray):
+    def __calculate_individual_fitness(self, gene: np.ndarray):
         one_hot_distances = np.zeros(
-            (self.gene_size, self.gene_size), dtype=np.float16
+            (self.gene_size, self.gene_size), dtype=np.int8
         )
         for i in np.arange(self.gene_size - 1).tolist():
             one_hot_distances[gene[i], gene[i + 1]] = 1
@@ -166,41 +169,60 @@ class TSP(object):
         return np.nansum(one_hot_distances * self.city_data.distances)
 
     def __calculate_population_fitness(self, population: np.ndarray):
-        population_fitness = np.full(self.population_size, 0, dtype=np.int8)
-        for i in np.arange(self.population_size).tolist:
-            population_fitness[i] = self.__calculate_fitness(population[i])
+        population_fitness = np.full(self.population_size, 0, dtype=np.float32)
+        for i in np.arange(self.population_size).tolist():
+            population_fitness[i] = self.__calculate_individual_fitness(
+                population[i]
+            )
 
         return population_fitness
 
-    def __selection(self):
+    def __calculate_fitness(self):
+        fitness = np.zeros(
+            (self.population_number, self.population_size), dtype=np.float32
+        )
         for i in np.arange(self.population_number).tolist():
-            self.__selection_on_population(self.populations[i])
+            fitness[i] = self.__calculate_population_fitness(
+                self.populations[i]
+            )
 
-    def __selection_on_population(self, population: np.ndarray):
+        return fitness
+
+    def __selection_on_population(
+        self, population: np.ndarray, population_fitness: np.ndarray
+    ):
         """
         Selection strategy used is based on fitness to allow maximum
         parallelization. Roulette and tournament could be considered,
         although it's expected to not perform as fast
 
         """
-        population_fitness_sorted = np.argsort(
-            self.__calculate_fitness(population)
-        )
-        fittest_individuals = population[
-            population_fitness_sorted[: self.__MAX_SELECTION_THRESHOLD]
-        ]
-        for not_fittest in population_fitness_sorted[
-            self.__MAX_SELECTION_THRESHOLD :  # noqa: E203
-        ]:
-            population[not_fittest] = fittest_individuals[
-                np.random.choice(self.__MAX_SELECTION_THRESHOLD, 1)[0]
-            ]
+        population_fitness_sorted = np.argsort(population_fitness)
+        # fittest_individuals = population[
+        #    population_fitness_sorted[: self.__MAX_SELECTION_THRESHOLD]
+        # ]
 
-    def __select_parents(self):
-        pass
+        return population_fitness_sorted[: self.__MAX_SELECTION_THRESHOLD]
 
-    def __crossover(self, popoulation: np.ndarray):
-        pass
+    def __get_crossover_parents(
+        self, popoulation: np.ndarray, fittest_invidivuals: np.ndarray
+    ):
+        parents = np.random.choice(fittest_invidivuals, 2, replace=False)
+        return popoulation[parents[0]], popoulation[parents[1]]
+
+    def __crossover(
+        self,
+        popoulation: np.ndarray,
+        mutation_rate: np.float16,
+        fittest_invidivuals: np.ndarray,
+    ):
+        for i in np.arange(self.population_size).tolist():
+            if i not in fittest_invidivuals:
+                parent_1, parent_2 = self.__get_crossover_parents(
+                    popoulation, fittest_invidivuals
+                )
+                popoulation[i] = self.__crossover_parents(parent_1, parent_2)
+                self.__mutate(popoulation[i], mutation_rate)
 
     def __crossover_parents(self, parent_1: np.ndarray, parent_2: np.ndarray):
         starting_cty = np.random.choice(self.gene_size, 1)[0]
@@ -246,31 +268,32 @@ class TSP(object):
             else destination_2
         )
 
-    def __mutate_genes(self, genes: np.ndarray, mutation_rate: np.float16):
-        genes_mutation_rates = np.random.uniform(0, 1, genes.shape)
-        genes_to_mutate = (genes_mutation_rates < mutation_rate).nonzero()
-
-        for i, gene in enumerate(genes_to_mutate):
-            genes[i] = self.__mutate_gene(gene)
-
-    def __mutate_gene(self, gene: np.ndarray):
-        chromosomes = np.random.choice(self.gene_size, 2, replace=False)
-        gene[[chromosomes[0], chromosomes[1]]] = gene[
-            [chromosomes[1], chromosomes[0]]
-        ]
+    def __mutate(self, individual: np.ndarray, mutation_rate: np.float16):
+        invidivual_mutation_rate = np.random.uniform(0, 1, 1)[0]
+        if invidivual_mutation_rate <= mutation_rate:
+            genes_to_mutate = np.random.choice(self.gene_size, 2, replace=False)
+            individual[[genes_to_mutate[0], genes_to_mutate[1]]] = individual[
+                [genes_to_mutate[1], genes_to_mutate[0]]
+            ]
 
     def run(self):
-        distance = self.__calculate_fitness(
-            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], self.city_data.distances
-        )
-        print(f"test distance is: {distance}")
-        # raise NotImplemented("run not implemented")
+        self.__initialize_populations()
+
+        for _ in np.arange(self.generation_number).tolist():
+            fitness = self.__calculate_fitness()
+            print(fitness)
+            for i in np.arange(self.population_number).tolist():
+                population = self.populations[i]
+                population_fitness = fitness[i]
+                mutation_rate = self.populations_mutation_rate[i]
+                fittest_individuals = self.__selection_on_population(
+                    population, population_fitness
+                )
+                self.__crossover(population, mutation_rate, fittest_individuals)
 
 
 def main():
-    tsp_base = TSP(
-        generation_number=5, population_number=5, num_cities=10, seed=42
-    )
+    tsp_base = TSP()
     tsp_base.run()
 
 
