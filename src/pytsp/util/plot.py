f872852224ea1a1e966e4dfc9151dabd8b26f593
Santiago Diez <sdsantiagodiez@gmail.com>
2022-02-27 21:33:41 -0300

    added bounds and angle approximation

--- src/pytsp/util/plot.py
+++ src/pytsp/util/plot.py
@@ -3,7 +3,9 @@ from folium import Map
 from folium import Marker
 from folium import FeatureGroup
 from folium.vector_layers import PolyLine
+from folium import RegularPolygonMarker
 import numpy as np
+import math
 
 
 class Mapplot(object):
@@ -15,7 +17,7 @@ class Mapplot(object):
         line_color: str = "blue",
         line_weight: int = 2,
     ):
-        initial_location = coordinates[0]
+        initial_location = tuple(coordinates[0])
 
         figure = Figure(width=width, height=height)
         map = Map(
@@ -24,29 +26,60 @@ class Mapplot(object):
             location=initial_location,
         )
 
-        # map.fit_bounds(_get_bounds(coordinates))
-
         for i in range(len(coordinates)):
 
             Marker(tuple(coordinates[i])).add_to(map)
 
         feature_group_name = "benchmark path"
         feature = FeatureGroup(feature_group_name)
-        # folium.RegularPolygonMarker(location=initial_location,
-        #  fill_color='blue',
-        # number_of_sides=3, radius=10, rotation=35).add_to(map) #arrow
+        rotation = Mapplot._get_degrees_approximation(
+            coordinates[0], coordinates[1]
+        )
+        RegularPolygonMarker(
+            location=initial_location,
+            fill_color="blue",
+            number_of_sides=3,
+            radius=10,
+            rotation=rotation,
+        ).add_to(map)
         PolyLine(coordinates, color=line_color, weight=line_weight).add_to(
             feature
         )
 
         feature.add_to(map)
-
         figure.add_child(map)
+
+        map.fit_bounds(Mapplot._get_bounds(coordinates))
         return figure
 
     @staticmethod
-    def _get_bounds(coordinates: list):
-        nw = coordinates.max()
-        se = coordinates.min()
+    def _get_degrees_approximation(coordinates_origin, coordinates_destination):
+        lat1 = math.radians(coordinates_origin[0])
+        lat2 = math.radians(coordinates_destination[0])
 
-        return [se, nw]
+        diffLong = math.radians(
+            coordinates_destination[1] - coordinates_origin[1]
+        )
+
+        x = math.sin(diffLong) * math.cos(lat2)
+        y = math.cos(lat1) * math.sin(lat2) - (
+            math.sin(lat1) * math.cos(lat2) * math.cos(diffLong)
+        )
+
+        initial_bearing = math.atan2(x, y)
+
+        initial_bearing = math.degrees(initial_bearing)
+        compass_bearing = (initial_bearing + 360) % 360
+
+        return compass_bearing
+
+    @staticmethod
+    def _get_bounds(coordinates: np.ndarray):
+        southests = coordinates.transpose()[0].min()
+        northests = coordinates.transpose()[0].max()
+        westests = coordinates.transpose()[1].min()
+        eastest = coordinates.transpose()[1].max()
+
+        sw = [southests, westests]
+        ne = [northests, eastest]
+        return [sw, ne]
